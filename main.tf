7cda5951d9f9ac739158362c9ce5cdd0f4065e07
omkarTap <102751132+omkarTap@users.noreply.github.com>
2025-07-16 11:26:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,13 +4,5 @@ provider "aws" {
 
 resource "aws_s3_bucket" "example" {
   bucket = "my-terraform-jenkins-bucket"
-  acl    = "private"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "my-tf-state-bucket"
-    key    = "state/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
